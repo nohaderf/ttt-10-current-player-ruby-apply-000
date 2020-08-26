768f001c44323b60339a7a01eedc6b406118dc10
Freda Hon <freda.hon@gmail.com>
2020-08-26 04:42:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -60,7 +60,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if !turn_count(board) % 2 == 0
+    puts "X's turn"
+  else
     puts "O's turn"
+    
   end
 end
