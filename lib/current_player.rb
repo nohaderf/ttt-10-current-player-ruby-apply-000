377a6fa3347e7b17f6ffc9a89a73cf620861f693
Freda Hon <freda.hon@gmail.com>
2020-08-26 04:39:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -49,17 +49,3 @@ def play(board)
   end
 end
 
-def turn_count(board)
-  counter = 0
-  board.each do |spot|
-    if spot == "X" || spot == "O"
-      counter += 1
-  end
-  counter
-end
-
-def current_player(board)
-  if turn_count(board) % 2 == 0
-    puts "O's turn"
-  end
-end
